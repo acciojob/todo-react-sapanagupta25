b1f67a771191477fe16abb0c03e1f6d49519eb81
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-04-26 14:31:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+  const onAdd = () => {
+    setTasks((prev) => [...prev, input]);
+    setInput("");
+  };
+
+  const onDelet = (ind) => {
+    const newTasks = [...tasks];
+    newTasks.splice(ind, 1);
+    setTasks(newTasks);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <p>To-Do List</p>
 
-export default App
+      <div>
+        <input value={input} onChange={onInput} />
+        <button onClick={onAdd}>Add Todo</button>
+      </div>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <span>{item}</span>
+            <button onClick={() => onDelet(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
